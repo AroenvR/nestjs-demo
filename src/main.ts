d011a00d3fe7b598fa52a0a161fdae7b0213eeab
AroenvR <102761367+AroenvR@users.noreply.github.com>
2024-09-29 19:23:27 +0000

    style: Minor adjustments

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,33 @@ import { AppModule } from './AppModule';
 import { LogAdapter } from './logging/LogAdapter';
 
 async function bootstrap() {
-	// TODO's:
-	/*
-        Implement middlewares for cors, helmet, rate limit, sanitizing, JWT validation, cookie validation
+    // TODO's:
+    /*
+        Implement JWT authentication
+        Create middlewares for:
+        - Validating JWT tokens
+        - Rate limiting
+        - Sanitizing input
+        - CORS
+        - Helmet
+        - Cookie validation
+        
+        Create Websockets
+        Create a microservice for RabbitMQ
         Implement a Task manager
         Implement a Queue manager
-        Implement a few abstract classes such as for DTO's and Entities
         Implement a validator utility
         Implement worker threads
         Implement a custom session storage
-        Create custom errors
     */
 
-	const app = await NestFactory.create(AppModule, {
-		bufferLogs: true,
-	});
+    const app = await NestFactory.create(AppModule, {
+        bufferLogs: true,
+    });
 
-	const logger = app.get(LogAdapter); // Retrieve the custom logger from Nest's DI container
-	app.useLogger(logger); // Set the custom logger for the entire application
+    const logger = app.get(LogAdapter); // Retrieve the custom logger from Nest's DI container
+    app.useLogger(logger); // Set the custom logger for the entire application
 
-	await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
