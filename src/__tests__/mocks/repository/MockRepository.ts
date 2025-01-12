8f371a2b882d60d6f13ed146fd1a5ea8c7efe130
AroenvR <102761367+AroenvR@users.noreply.github.com>
2025-01-12 19:07:57 +0000

    tests: Updated MockRepo to include a findOne query

--- src/__tests__/mocks/repository/MockRepository.ts
+++ src/__tests__/mocks/repository/MockRepository.ts
@@ -1,12 +1,26 @@
 import { AbstractEntity } from '../../../domain/AbstractEntity';
 
 /**
+ * Criteria to execute a FindOne query.
+ */
+type TFindOneQuery = {
+	where: Record<string, string | number>;
+	relations: string[];
+};
+
+/**
  * Mocks a repository with CRUD operations.
  */
 export class MockRepository<Entity extends AbstractEntity> {
 	constructor(private createEntity: () => Entity) {}
 
 	find = jest.fn().mockResolvedValue([this.createEntity()]);
+
+	findOne = jest.fn().mockImplementation((query: TFindOneQuery) => {
+		if (query.where.id === 69) return null; // To test the error handling
+
+		return this.createEntity();
+	});
 
 	findOneBy = jest.fn().mockImplementation((criteria: Partial<Entity>) => {
 		if (criteria.id === 69) return null; // To test the error handling
