c720ad5807edc0be0248b67722f83bdbe8b4663d
AroenvR <102761367+AroenvR@users.noreply.github.com>
2025-01-17 19:39:14 +0000

    formatted

--- src/http_api/middleware/LoggerMiddleware.integration.test.ts
+++ src/http_api/middleware/LoggerMiddleware.integration.test.ts
@@ -11,77 +11,77 @@ import express, { Express } from 'express';
 const TEST_NAME = 'LoggerMiddleware_Integration';
 
 describe(TEST_NAME, () => {
-    const ENDPOINT = '/test';
-    let app: Express;
+	const ENDPOINT = '/test';
+	let app: Express;
 
-    beforeAll(async () => {
-        process.env.TEST_NAME = TEST_NAME;
+	beforeAll(async () => {
+		process.env.TEST_NAME = TEST_NAME;
 
-        const module: TestingModule = await Test.createTestingModule({
-            imports: [
-                ConfigModule.forRoot({
-                    isGlobal: true,
-                    load: [serverConfig],
-                }),
-                LoggerModule,
-            ],
-            providers: [LoggerMiddleware],
-        }).compile();
+		const module: TestingModule = await Test.createTestingModule({
+			imports: [
+				ConfigModule.forRoot({
+					isGlobal: true,
+					load: [serverConfig],
+				}),
+				LoggerModule,
+			],
+			providers: [LoggerMiddleware],
+		}).compile();
 
-        const middleware = module.get<LoggerMiddleware>(LoggerMiddleware);
-        app = express();
-        app.use(express.json());
-        app.use(middleware.use.bind(middleware));
+		const middleware = module.get<LoggerMiddleware>(LoggerMiddleware);
+		app = express();
+		app.use(express.json());
+		app.use(middleware.use.bind(middleware));
 
-        app.get(ENDPOINT, (req, res) => {
-            res.status(200).send('Success');
-        });
-    });
+		app.get(ENDPOINT, (req, res) => {
+			res.status(200).send('Success');
+		});
+	});
 
-    // --------------------------------------------------
+	// --------------------------------------------------
 
-    it('logs requests and responses', async () => {
-        await request(app).get(ENDPOINT);
+	it('logs requests and responses', async () => {
+		await request(app).get(ENDPOINT);
 
-        await expect(wasLogged(TEST_NAME, `LOG - LoggerMiddleware: Request: GET ${ENDPOINT}`)).resolves.toBe(true);
+		await expect(wasLogged(TEST_NAME, `LOG - LoggerMiddleware: Request: GET ${ENDPOINT}`)).resolves.toBe(true);
 
-        await expect(wasLogged(TEST_NAME, `LOG - LoggerMiddleware: Response: GET ${ENDPOINT} - Status: 200`)).resolves.toBe(true);
-    });
+		await expect(wasLogged(TEST_NAME, `LOG - LoggerMiddleware: Response: GET ${ENDPOINT} - Status: 200`)).resolves.toBe(true);
+	});
 
-    // --------------------------------------------------
+	// --------------------------------------------------
 
-    it('logs request and response cycles with a provided correlation Id', async () => {
-        const correlationId = randomUUID();
-        await request(app).get(ENDPOINT).set('X-Correlation-ID', correlationId);
+	it('logs request and response cycles with a provided correlation Id', async () => {
+		const correlationId = randomUUID();
+		await request(app).get(ENDPOINT).set('X-Correlation-ID', correlationId);
 
-        await expect(wasLogged(TEST_NAME, `${correlationId} LOG - LoggerMiddleware: Request: GET ${ENDPOINT}`)).resolves.toBe(true);
-        await expect(wasLogged(TEST_NAME, `${correlationId} LOG - LoggerMiddleware: Response: GET ${ENDPOINT} - Status: 200`)).resolves.toBe(true);
-    });
+		await expect(wasLogged(TEST_NAME, `${correlationId} LOG - LoggerMiddleware: Request: GET ${ENDPOINT}`)).resolves.toBe(true);
+		await expect(wasLogged(TEST_NAME, `${correlationId} LOG - LoggerMiddleware: Response: GET ${ENDPOINT} - Status: 200`)).resolves.toBe(true);
+	});
 
-    // --------------------------------------------------
+	// --------------------------------------------------
 
-    it('logs request headers, query, params, and body when truthy', async () => {
-        const payload = {
-            query: { test: 'queryValue' },
-            params: { id: '123' },
-            body: { key: 'value' },
-        };
+	it('logs request headers, query, params, and body when truthy', async () => {
+		const payload = {
+			query: { test: 'queryValue' },
+			params: { id: '123' },
+			body: { key: 'value' },
+		};
 
-        app.get('/test/:id', (req, res) => {
-            res.status(200).send('Success');
-        });
+		app.get('/test/:id', (req, res) => {
+			res.status(200).send('Success');
+		});
 
-        await request(app).get('/test/123').query(payload.query).set('Content-Type', 'application/json').set('host', '127.0.0.69').send(payload.body);
+		await request(app).get('/test/123').query(payload.query).set('Content-Type', 'application/json').set('host', '127.0.0.69').send(payload.body);
 
-        await expect(
-            wasLogged(
-                TEST_NAME,
-                `VERBOSE - LoggerMiddleware: Request Headers: {"host":"127.0.0.69","accept-encoding":"gzip, deflate","content-type":"application/json","content-length":"15","connection":"close"}`,
-            ),
-        ).resolves.toBe(true);
+		await expect(
+			wasLogged(
+				TEST_NAME,
+				`VERBOSE - LoggerMiddleware: Request Headers: {"host":"127.0.0.69","accept-encoding":"gzip, deflate","content-type":"application/json","content-length":"15","connection":"close"}`,
+			),
+		).resolves.toBe(true);
 
-        await expect(wasLogged(TEST_NAME, `VERBOSE - LoggerMiddleware: Request Body: ${JSON.stringify(payload.body)}`)).resolves.toBe(true);
+		await expect(wasLogged(TEST_NAME, `VERBOSE - LoggerMiddleware: Request Body: ${JSON.stringify(payload.body)}`)).resolves.toBe(true);
 
-        await expect(wasLogged(TEST_NAME, `VERBOSE - LoggerMiddleware: Request Query: ${JSON.stringify(payload.query)}`)).resolves.toBe(true);
-    });
+		await expect(wasLogged(TEST_NAME, `VERBOSE - LoggerMiddleware: Request Query: ${JSON.stringify(payload.query)}`)).resolves.toBe(true);
+	});
 });
