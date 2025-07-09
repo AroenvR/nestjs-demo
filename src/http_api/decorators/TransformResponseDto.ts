4f7881131a71d10a3be4097f419ddf1df43c752b
AroenvR <102761367+AroenvR@users.noreply.github.com>
2025-07-09 14:38:38 +0000

    update: Improved the Transform DTO Interceptor to work as a Controller's decorator instead of a controller's function decorator

--- src/http_api/decorators/TransformResponseDto.ts
+++ src/http_api/decorators/TransformResponseDto.ts
@@ -1,11 +1,13 @@
-import { applyDecorators, UseInterceptors } from "@nestjs/common";
+import { applyDecorators, SetMetadata, UseInterceptors } from "@nestjs/common";
 import { TransformResponseInterceptor } from "../interceptors/TransformResponseInterceptor";
 
+export const RESPONSE_DTO_KEY = "responseDto";
+
 /**
- * Decorator to apply the TransformResponseInterceptor to a controller method.
+ * Decorator to apply the TransformResponseInterceptor to controller's endpoints.
  * This decorator is used to transform the response data into an instance of a specified DTO.
- * It can be applied to any controller method that returns a DTO.
+ * It can be applied to any controller or controller method that returns data which fits the specified DTO.
  */
 export function TransformResponseDto(dto: unknown) {
-	return applyDecorators(UseInterceptors(new TransformResponseInterceptor(dto)));
+	return applyDecorators(SetMetadata(RESPONSE_DTO_KEY, dto), UseInterceptors(TransformResponseInterceptor));
 }
--- src/http_api/interceptors/TransformResponseInterceptor.ts
+++ src/http_api/interceptors/TransformResponseInterceptor.ts
@@ -1,6 +1,9 @@
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, InternalServerErrorException } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { SSE_METADATA_KEY } from "../decorators/SseEndpoint";
+import { RESPONSE_DTO_KEY } from "../decorators/TransformResponseDto";
 
 /**
  * An interceptor that transforms the response data into an instance of a specified class.
@@ -10,20 +13,23 @@ import { Injectable, NestInterceptor, ExecutionContext, CallHandler, InternalSer
  */
 @Injectable()
 export class TransformResponseInterceptor implements NestInterceptor {
-	constructor(private readonly targetClass: any) {}
+	constructor(private readonly reflector: Reflector) {}
 
-	intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const isSse = this.reflector.get<boolean>(SSE_METADATA_KEY, context.getHandler());
+		if (isSse) return next.handle();
+
+		const handler = context.getHandler();
+		const classRef = context.getClass();
+
+		const dto = this.reflector.get(RESPONSE_DTO_KEY, handler) ?? this.reflector.get(RESPONSE_DTO_KEY, classRef);
+		if (!dto) return next.handle();
+
 		return next.handle().pipe(
 			map((data) => {
-				if (!data || !this.targetClass) return data;
-
-				// Handle arrays
-				if (Array.isArray(data)) {
-					return data.map((item) => this.transformSingleItem(item));
-				}
-
-				// Handle single objects
-				return this.transformSingleItem(data);
+				if (!data || !dto) return data;
+				if (Array.isArray(data)) return data.map((item) => this.transformSingleItem(item, dto));
+				return this.transformSingleItem(data, dto);
 			}),
 		);
 	}
@@ -33,14 +39,12 @@ export class TransformResponseInterceptor implements NestInterceptor {
 	 * @param data The data to transform, which should be an object or a primitive value.
 	 * @returns An instance of the target class created from the provided data.
 	 */
-	private transformSingleItem(data: any): any {
-		// Check if the target class has a static 'create' method
-		if (typeof this.targetClass.create !== "function") {
-			throw new InternalServerErrorException(
-				`${this.constructor.name}: No static 'create' method found on target class ${this.targetClass.name}.`,
-			);
+	private transformSingleItem(data: any, dtoClass: any): any {
+		if (typeof dtoClass.create !== "function") {
+			throw new InternalServerErrorException(`${this.constructor.name}: No static 'create' method found on target class ${dtoClass.name}.`);
 		}
 
-		return this.targetClass.create(data);
+		if (data.isDto === true) return data;
+		return dtoClass.create(data);
 	}
 }
